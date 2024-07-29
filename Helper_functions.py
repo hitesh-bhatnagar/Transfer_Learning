c964bbe2af05f6fd7b1e80c1e5de653350259538
Xenon917 <hitesh.bhatnagar20@gmail.com>
2024-07-29 10:49:58 +0530

    Added Helper_functions

---
+++ Helper_functions.py
@@ -0,0 +1,221 @@
+import tensorflow as tf
+
+# create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale = True):
+
+  # read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+
+## Note: The below confusion matrix code is a remix of Scikit-Learn's
+
+# plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
+
+import itertools
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Our function needs a different name to sklearn's plot_confusion_matrix
+
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False):
+
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]  # normalize it
+
+  n_classes = cm.shape[0]       # find the number of classes we're dealing with
+
+  # Plot the figure and make it the good
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues)  # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
+
+  # Are there a list of classes?
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes),       # create enough axis slots for each class
+         yticks=np.arange(n_classes),
+         xticklabels=labels,          # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+
+
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+
+    # Return the figure
+  return fig
+
+# Make a function to predict on images and plot them (works with multi-glass)
+
+def pred_and_plot(model, filename, class_names):
+
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  if len(pred[0]) > 1: # check for multi-class
+    pred_class = class_names[pred.argmax()] # if more than one output, take the max
+
+  else:
+    pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
+
+  # Plot the image and predicted class
+  plt.imshow(img)
+  plt.title(f"Prediction: {pred_class}")
+  plt.axis(False)
+
+import datetime
+
+def create_tensorboard_callback(dir_name, experiment_name):
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(
+      log_dir=log_dir
+  )
+
+  print(f"Saving TensorBoard log files to: {log_dir}")
+  return tensorboard_callback
+
+
+# Plot the validation and training data separately
+
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+  """
+
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+
+  epochs = range(len(history.history["loss"]))
+
+  # Plot loss
+  plt.plot(epochs, loss, label="training_loss")
+  plt.plot(epochs, val_loss, label="val_loss")
+  plt.title("Loss")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label="training_accuracy")
+  plt.plot(epochs, val_accuracy, label="val_accuracy")
+  plt.title("Accuracy")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+def compare_historys(original_history, new_history, initial_epochs=5):
+
+  # get original history measurements
+  acc = original_history.history["accuracy"]
+  loss = original_history.history["loss"]
+
+  val_acc = original_history.history["val_accuracy"]
+  val_loss = original_history.history["val_loss"]
+
+  # combine original history with new history
+  total_acc = acc + new_history.history["accuracy"]
+  total_loss = loss + new_history.history["loss"]
+
+  total_val_acc = val_acc + new_history.history["val_accuracy"]
+  total_val_loss = val_loss + new_history.history["val_loss"]
+
+  # Make plots
+  plt.figure(figsize=(8, 8))
+  plt.subplot(2, 1, 1)
+  plt.plot(total_acc, label='Training Accuracy')
+  plt.plot(total_val_acc, label='Validation Accuracy')
+  plt.plot([initial_epochs-1, initial_epochs-1],
+          plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+  plt.legend(loc='lower right')
+  plt.title('Training and Validation Accuracy')
+
+  plt.subplot(2, 1, 2)
+  plt.plot(total_loss, label='Training Loss')
+  plt.plot(total_val_loss, label='Validation Loss')
+  plt.plot([initial_epochs-1, initial_epochs-1],
+          plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+  plt.legend(loc='upper right')
+  plt.title('Training and Validation Loss')
+  plt.xlabel('epoch')
+  plt.show()
+
+
+# Create a function to unzip a zipfile into current working directory
+# (since we're going to be downloading and unzipping from a url)
+
+import zipfile
+
+def unzip_data(filename):
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+
+
+# Walk through an image classification directory and find out how many files (images)
+# are in each subdirectory.
+
+import os
+
+def walk_through_dir(dir_path):
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def calculate_resultst(y_true, y_pred):
+
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+
+  return model_results
+
